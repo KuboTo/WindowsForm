2c5f44316ffba19335f21b874ca65db7ef8be492
KuboTo <wangdengcheng1993@163.com>
2017-05-04 11:11:07 +0800

    WindowForm_Test
    Windows窗体应用程序测试

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources._15510691582821131539;
+            this.ClientSize = new System.Drawing.Size(500, 282);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.HelpButton = true;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "天降之物";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Click += new System.EventHandler(this.MainForm_Click);
+            this.DoubleClick += new System.EventHandler(this.MainForm_DoubleClick);
+            this.MouseHover += new System.EventHandler(this.MainForm_MouseHover);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //事件的本质就是一种方法
+            //MessageBox.Show("窗体加载完成");
+
+            this.Text = "天降之物";
+            //MainForm;.Text = "修改Text属性";
+
+            Button myButton = new Button();
+            myButton.Text = "伊卡洛斯";
+            this.Controls.Add(myButton);
+
+
+        }
+
+        private void MainForm_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("单击事件");//运行时，在窗口单击即可显示对应操作
+
+            //form1单击事件，显示form2
+            Form2 myForm2 = new Form2();
+            myForm2.Show();
+        }
+
+        private void MainForm_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("双击击事件");//运行时，在窗口双击即可显示对应操作
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("我要关闭窗口咯！！！");//关闭窗口时会出现提示
+        }
+
+        private void MainForm_MouseHover(object sender, EventArgs e)
+        {
+            this.Text = "鼠标悬停显示操作";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //在这个方法里面所有的设置都是通过单击button才能实现的
+
+            this.Text = "伊卡洛斯";//单击button获修改窗体Text不会修改button的Text
+
+            button1.Text = "伊卡洛斯";//单价button修改button的Text
+
+            //form1单击事件，显示form2
+            Form2 myForm2 = new Form2();
+            myForm2.Show();
+            
+            this.Hide();//点开form2的时候隐藏本form
+
+        }
+
+        
+    }
+}
